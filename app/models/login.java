d761f1ebd6b49b54c4a84650ce219bb8f1ce652c
unknown <seifu001@UMC-8470-1868.ad.umn.edu>
2014-01-14 02:23:04 -0600

    documented

--- app/models/login.java
+++ app/models/login.java
@@ -1,6 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Construct and accesor methods for log in object.
+ * Also, used for clearDB cloud database connection
+ * note: import play.db.DB isnt used but spared for future jpa test (which provides less mysql legging)
  */
 package models;
 import java.sql.Connection;
@@ -15,32 +16,63 @@ import play.db.DB;
 
 /**
  *
- * @author seifu001
+ * @author kirubel s.
  */
 public class login {
+    // validator
     @Required public String onlineID;
     @Required public String passcode;
     
+    //constructor, though java automatically creates one
     public login(){}
+    
+    /**constructor of each login attempt, helpful for multiple user access
+        @param String
+        @param String
+        @return none
+        */
     public login(String onlineID,String passcode){
         this.onlineID=onlineID;
         this.passcode=passcode;
     }
     
+    /** setter
+        @param String
+        @return none
+        */
     public void setOnlineID(String onlineID){
         this.onlineID=onlineID;
     }
+    
+    /** getter
+        @param String
+        @return String
+        */
     public String getOnlineID(){
         return this.onlineID;
     }
     
+    /** setter
+        @param String
+        @return none
+        */
     public void setPassCode(String passcode){
         this.passcode=passcode;
     }
+    
+    /** getter
+        @param String
+        @return String
+        */
     public String getPassCode(){
         return this.passcode;
     }
-   
+   /** recieves constructed login object from controller
+    * returns true/false if designated user object exists or not
+        @param String
+        @param String
+        @return boolean
+        */
     public boolean check(String onlineID,String passcode){
      
                 boolean verify=false;
@@ -48,7 +80,7 @@ public class login {
                 Statement stmt=null;
                 ResultSet rs=null;
                 try{
-                    //postgres url
+                    //fpr postgres url
                    //  String url = "jdbc:postgresql://ec2-54-221-206-165.compute-1.amazonaws.com:5432/d7uvtdoem30m6m?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"; 
                   
                   String url = "jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_98dc3582d5c864d?reconnect=true";
@@ -60,13 +92,16 @@ public class login {
                     stmt=conn.createStatement();
                     
                     String sql;
+                    
+                    //a postgres database query
                     //sql="SELECT Cust_ID,password from corebanking.customer";
                      sql="SELECT userID,password from heroku_98dc3582d5c864d.employee";
                      stmt.executeQuery(sql);
                      rs = stmt.getResultSet();
+                     
+                    //read each resultset 
                     while(rs.next()){
-                         int uID=rs.getInt("userID");
-                         //String ssn=rs.getString("SSN");
+                         int uID=rs.getInt("userID");                       
                          String pas=rs.getString("password");
                          String loginID=Integer.toString(uID);
                            
